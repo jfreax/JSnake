0a9764e6d80d02d576c7a333fd93034db3d1472e
Jens Dieskau <jfreax@googlemail.com>
2011-06-25 23:10:32 +0200

    Begin to use socket.io for multiplayer

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ console.log("Express server listening on port %d in %s mode", app.address().port
 
 
 io.sockets.on('connection', function(client) {
+  client.on('message', function( data ) {
 
+  });
 
 });
