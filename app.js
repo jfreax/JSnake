076cda2c833f57bc029cc37dc87198dfe29bccdb
Jens Dieskau <jfreax@googlemail.com>
2011-06-25 18:35:37 +0200

    Draw snake

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var express = require('express')
  */
 app.configure(function(){
   app.set('views', __dirname + '/views');
+  app.set('view options', {layout: false});
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
